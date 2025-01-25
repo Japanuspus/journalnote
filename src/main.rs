7fad24a97714a93a0989906b8a3517e1bf4b2acf
Janus <janus@insignificancegalore.net>
2025-01-25 21:46:09 +0100

    Add continuation feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use chrono;
 use clap::Parser;
 // use std::fmt::Write;
@@ -6,6 +7,7 @@ use std::env;
 use std::fs;
 use std::io::{BufRead, Read, Seek, SeekFrom, Write};
 use std::path;
+use std::path::PathBuf;
 
 fn format_date<D: chrono::Datelike>(d: &D) -> String {
     format!("{:04}-{:02}-{:02}", d.year(), d.month(), d.day())
@@ -31,11 +33,53 @@ struct Args {
     content: Vec<String>,
 }
 
-fn main() {
+struct Message<'a> {
+    is_continuation: bool,
+    content: &'a str,
+}
+
+/// Seek to just before terminating newline, or EOF if no such exists
+fn file_seek_existing(note_file: &PathBuf, day_header: &str) -> anyhow::Result<(fs::File, bool)> {
+    let mut existing_file = fs::OpenOptions::new()
+        .read(true)
+        .write(true) // not append - we do our own (non-atomic) seek
+        .open(&note_file)
+        .with_context(|| format!("Failed to open detected existing file {:?}", &note_file))?;
+    let has_today = std::io::BufReader::new(&existing_file)
+        .lines()
+        .any(|line| line.is_ok_and(|s| s.starts_with(&day_header)));
+
+    let mut buffer = [0; 1];
+    if existing_file.seek(SeekFrom::End(-1)).is_ok()
+        && existing_file.read(&mut buffer).is_ok()
+        && buffer[0] == b'\n'
+    {
+        let _ = existing_file.seek_relative(-1);
+    }
+    Ok((existing_file, has_today))
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let now = chrono::offset::Local::now();
     let today = now.date();
     let friday = today + chrono::Duration::days(days_till_friday(&today) as i64);
+
+    let args_joined = args.content.join("\n");
+    let message = {
+        let trimmed = args_joined.trim();
+        if trimmed.starts_with("...") {
+            Message {
+                is_continuation: true,
+                content: &trimmed[3..],
+            }
+        } else {
+            Message {
+                is_continuation: false,
+                content: &trimmed,
+            }
+        }
+    };
 
     let note_folder_str = env::var("JOURNAL_NOTE_FOLDER")
         .expect("Environment variable JOURNAL_NOTE_FOLDER must be defined");
@@ -50,71 +94,48 @@ fn main() {
 
     // Buffer for all additions to file, to make sure we get an atomic write.
     let mut buffer = String::new();
-    let mut has_today: bool = false;
-    let mut clean_line: bool = false; //
     let day_header = format!("## {}", format_date(&today));
 
-    let mut file = match fs::OpenOptions::new()
+    let (mut file, has_today, mut allow_continuation) = match fs::File::options()
         .write(true)
         .create_new(true)
         .open(&note_file)
     {
         Err(ref e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
-            let mut existing_file = fs::OpenOptions::new()
-                .read(true)
-                .append(true)
-                .open(&note_file)
-                .expect("Failed to open existing file");
-            for line in std::io::BufReader::new(&existing_file).lines() {
-                if line.expect("Invalid UTF8").starts_with(&day_header) {
-                    has_today = true;
-                    break;
-                }
-            }
-            {
-                let mut buffer = [0; 1];
-                existing_file
-                    .seek(SeekFrom::End(-1))
-                    .expect("Unable to seek to end of file");
-                match existing_file.read(&mut buffer) {
-                    Ok(1) => {
-                        if buffer[0] == b'\n' {
-                            clean_line = true;
-                        }
-                    }
-                    _ => panic!("Could not read end of file"),
-                }
-            }
-            existing_file
+            let (f, h) = file_seek_existing(&note_file, &day_header)?;
+            (f, h, true)
         }
         Ok(f) => {
             buffer.push_str(&format!(
-                "# Journal for week ending at {}\n\n",
+                "# Journal for week ending at {}\n",
                 format_date(&friday)
             ));
-            clean_line = true;
-            f
+            (f, false, false)
         }
-        _ => panic!("Failed to create new file"),
+        // Err(ref e) => return Err(e).with_context(|| format!("Unable to create journal file {:?}", &note_file))
+        _ => panic!("Unable to create journal file {:?}", &note_file),
     };
 
+    //Terminate these by only one newline. One more will be added with message.
     if !has_today {
-        buffer.push_str(&format!("\n{} - {}\n\n", &day_header, &today.format("%a")));
-        clean_line = true;
+        allow_continuation = false;
+        buffer.push_str(&format!("\n\n{} - {}\n", &day_header, &today.format("%a")));
+    }
+    if let Some(header) = args.header {
+        allow_continuation = false;
+        buffer.push_str(&format!("\n### {} - {}\n", &now.format("%H:%M"), header));
     }
 
-    if let Some(header) = args.header {
-        buffer.push_str(&format!("\n### {} - {}\n\n", &now.format("%H:%M"), header));
-        clean_line = true;
-    }
-    let message = args.content.join("\n");
-    if !message.is_empty() {
-        if !clean_line {
+    if !message.content.is_empty() {
+        if !(message.is_continuation && allow_continuation) {
             buffer.push_str("\n");
+        } else {
+            buffer.push_str(" ");
         }
-        buffer.push_str(&format!("{}\n", message));
+        buffer.push_str(&format!("{}\n", message.content));
     }
     // This write should be atomic...
     file.write_all(buffer.as_bytes())
         .expect("Unable to write to file");
+    Ok(())
 }
